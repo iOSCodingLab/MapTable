3ce8c921590b83b7fa8e9f345b40587dce7dc7e2
Ricardo Michel Reyes <richie.1993@yahoo.co.uk>
2014-03-11 09:42:15 -0500

    Initial commit

---
+++ MapTableTests/MapTableTests.m
@@ -0,0 +1,34 @@
+//
+//  MapTableTests.m
+//  MapTableTests
+//
+//  Created by Laboratorio San Agustin on 8/03/14.
+//  Copyright (c) 2014 Online Studio Productions LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapTableTests : XCTestCase
+
+@end
+
+@implementation MapTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
